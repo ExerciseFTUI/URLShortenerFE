55f9a1658b5cc4a6c50306151312525101ff4345
Den <dimas.dermawan@ui.ac.id>
2023-07-13 13:43:34 +0700

    FIX bugs and ADD Button Component

---
+++ src/components/button/ButtonLink.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom"
+
+function ButtonLink({
+  to = "",
+  title = "Button",
+  theme = "dark",
+  width = "fit",
+  className = "",
+}) {
+  return (
+    // prettier-ignore
+    <Link 
+      to={to} 
+      className={
+        "font-semibold py-2 hover:scale-95 ease-in-out duration-200 rounded-md text-center text-lg " +
+        (theme == "dark" ? "bg-dark-2 " : "bg-light ") +
+        (theme == "dark" ? "text-light " : "text-dark-1 ") +
+        (`w-${width} `) +
+        className
+      }
+    >
+      {title}
+    </Link>
+  )
+}
+
+export default ButtonLink
