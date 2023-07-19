92c2b254ce663e55b83d617a08a1dca237005d39
Den <dimas.dermawan@ui.ac.id>
2023-07-19 15:24:13 +0700

    EDIT button style

--- src/components/button/ButtonLink.jsx
+++ src/components/button/ButtonLink.jsx
@@ -12,7 +12,7 @@ function ButtonLink({
     <Link 
       to={to} 
       className={
-        "font-semibold py-2 hover:scale-95 ease-in-out duration-200 rounded-md text-center text-lg " +
+        "font-semibold hover:scale-95 ease-in-out duration-200 rounded-md text-center text-lg " +
         (theme == "dark" ? "bg-dark-2 " : "bg-light ") +
         (theme == "dark" ? "text-light " : "text-dark-1 ") +
         (`w-${width} `) +
