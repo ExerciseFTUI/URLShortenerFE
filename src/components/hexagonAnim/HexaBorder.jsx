d476d58821f41f1b9d098a130b1d46cdc8c13cbf
Den <dimas.dermawan@ui.ac.id>
2023-07-29 22:52:56 +0700

    More customizable

--- src/components/hexagonAnim/HexaBorder.jsx
+++ src/components/hexagonAnim/HexaBorder.jsx
@@ -2,17 +2,19 @@ import React from "react"
 
 import { motion } from "framer-motion"
 
-import hexaBord from "../../assets/hexagonals/hexa-border.svg"
+import light from "../../assets/hexagonals/hexa-border.svg"
+import dark from "../../assets/hexagonals/hexa-border-dark.svg"
 
 function HexaBorder({
   duration = 8,
   initial = Math.random() * 100,
   className = "",
+  theme = "light",
 }) {
   return (
     <motion.img
+      src={theme == "light" ? light : dark}
       alt="Hexagonal"
-      src={hexaBord}
       className={className}
       initial={{
         rotate: initial,
