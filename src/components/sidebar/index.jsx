7ed6a731d0b74bb773498bdec42177ac181f8ab2
Den <dimas.dermawan@ui.ac.id>
2023-07-30 00:51:54 +0700

    Profile picture

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,5 +1,8 @@
-import { useState } from "react"
+import { AnimatePresence, motion } from "framer-motion"
+import { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
+
+import logo from "../../assets/exe-logo-with-bg.png"
 
 const navs = [
   { text: "History Links", to: "/url-shortener/history", path: "history" },
@@ -9,18 +12,33 @@ const navs = [
 
 function Sidebar() {
   const location = useLocation().pathname.split("/")
+  const nameStrg = sessionStorage.getItem("name")
 
   const [opened, setOpened] = useState(false)
+  const [account, setAccount] = useState(false)
+  const [userName, setUserName] = useState("")
 
   function toggleSide() {
     setOpened(!opened)
   }
 
+  function handleLogOut() {}
+
+  useEffect(() => {
+    try {
+      setUserName(nameStrg.split(" ")[1])
+    } catch {
+      console.log()
+    }
+
+    if (!userName) setUserName("Namasaya")
+  }, [nameStrg])
+
   // prettier-ignore
   return (
     <aside
       id="sidebar"
-      className={`bg-dark-1 text-lg fixed top-0 h-full px-8 py-16 flex flex-col justify-between z-20 border-grey-2 border-r-2 ease-in-out duration-500 ${opened ? "left-0" : "-left-[184px]"}`}
+      className={`bg-dark-1 text-lg fixed top-0 h-full px-8 py-16 flex flex-col justify-between z-20 border-grey-2 border-r-2 ease-in-out duration-500 ${opened ? "left-0" : "-left-[194px]"}`}
     >
       <div
         onClick={toggleSide}
@@ -46,8 +64,8 @@ function Sidebar() {
       </div>
 
       <div className="overflow-hidden ease-in-out duration-300">
-        <Link to="/summary" className="btn-light rounded-md mb-4">
-          Create New
+        <Link to="/summary" className="btn-light rounded-md mb-4 w-full">
+          Home
         </Link>
 
         <div className="bg-light w-full h-[1px] mb-6" />
@@ -66,10 +84,43 @@ function Sidebar() {
           ))}
         </ul>
       </div>
+      
+      <div className="pointer-events-auto w-32">
+        <button 
+          title="user"
+          type="button"
+          onClick={() => setAccount(!account)}
+          className="bg-light w-full px-4 py-2 rounded-md flex items-center justify-between gap-2 border-b-2 border-dark-2 relative z-[1]"
+        >
+          <img alt="" src={logo} className="w-8 h-8 rounded-full border-dark-2 border-2" />
 
-      <Link to="/account" className="btn-light rounded-md">
-        Nama User
-      </Link>
+          <h1 className="text-dark-2 text-base overflow-hidden">{userName}</h1>
+        </button>
+
+        <AnimatePresence>
+        { account &&   
+            <motion.div
+              initial={{ scaleY: 0 }}
+              animate={{ scaleY: 1 }}
+              exit={{ scaleY: 0 }}
+              className="text-dark-2 text-center flex flex-col justify-center items-center -mt-1 origin-top"
+            >
+            <Link to="/account" className="bg-light w-full py-2 font-semibold border-b-2 border-dark-2 rounded-b-sm">
+                Account
+            </Link>
+
+            <button 
+              title="log out" 
+              type="button"
+              onClick={handleLogOut} 
+              className="bg-light w-full py-2 font-semibold rounded-b-md"
+            >
+                Log Out
+            </button>
+          </motion.div>
+        }
+        </AnimatePresence>
+      </div>
     </aside>
   )
 }
